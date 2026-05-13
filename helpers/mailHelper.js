e38383849199e6bef8a2fbe87a5f65cd6d912536
Mikita Zhyhadla <nikizihadlo93@gmail.com>
2026-05-13 23:52:40 +0200

    refactor: address PR review comments
    - Switch project to ESM, convert all files from require/module.exports to import/export
    - Replace Logger class with test.step() for step reporting, delete utils/logger.js
    - Move generateEmail from MailHelper into test-data/jobSubscribe.js, delete helpers/mailHelper.js
    - Add searchWithFallback() to JobSearchPage, encapsulate retry logic from test
    - Simplify clickFirstJob() to single selector with lightweight retry
    - Scope job list selector to ul.job-list container for specificity
    - Remove redundant clearLocation() call (field is empty by default)
    - Remove try/catch wrappers around assertions, use expect directly
    - Group related actions and assertions into shared test.step() blocks in scenario2

--- helpers/mailHelper.js
+++
@@ -1,9 +0,0 @@
-class MailHelper {
-  static generateUniqueEmail(prefix = 'test.ikea') {
-    const timestamp = Date.now();
-    const randomSuffix = Math.random().toString(36).substring(2, 7);
-    return `${prefix}.${timestamp}.${randomSuffix}@testmail.dev`;
-  }
-}
-
-module.exports = MailHelper;
